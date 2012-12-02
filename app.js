e9abe6bb1ceb491b526fd38c15f093176d76ebc5
Blake Taylor <blakefrost@gmail.com>
2012-12-02 01:23:21 -0800

    Add some fancy geohashing

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var Firebase = require('./firebase-node');
 
-var db = new Firebase('https://io.firebaseio.com');
+var db = new Firebase('https://io.firebaseio.com/items');
+var geohash = require('ngeohash');
 
 console.log("Listening for changes");
 
-db.on("value", function(v){
-  console.log(v.val());
+db.on("child_added", function(snapshot){
+  var value = snapshot.val(),
+      lat   = value.lat,
+      lon   = value.lon,
+      hash  = geohash.encode(lat, lon);
+
+  snapshot.ref().update({geohash: hash});
+  snapshot.ref().setPriority(hash);
+
+
+  //console.log(value);
+  //console.log(lat);
+  //console.log(lon);
+  //console.log(hash);
+  //console.log(geohash.encode(37.8324, 112.5584));
+  //console.log(v.val());
 });
+
+
+
 
 //db.child("blake").set('from me');
 
@@ -21,4 +39,10 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+
+// Get some stuff
+db.startAt("ww8p1r4t").endAt("ww8p1r4u").on("value", function(snap) {
+  console.log(snap.val());
 });
