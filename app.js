6cb6864cc27dbb7122d9c6a945ca5fd1631c5134
Blake Taylor <blakefrost@gmail.com>
2012-12-01 23:42:58 -0800

    Add a web server

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ var db = new Firebase('https://io.firebaseio.com');
 console.log("Listening for changes");
 
 db.on("value", function(v){
-
   console.log(v.val());
 });
 
 //db.child("blake").set('from me');
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
