87f0a9d54dbcfe89206738f0026f77fc52212261
Blake Taylor <blakefrost@gmail.com>
2012-12-01 23:50:47 -0800

    Turn off webserver

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ db.on("value", function(v){
 
 //db.child("blake").set('from me');
 
-var express = require('express');
+//var express = require('express');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+//app.get('/', function(request, response) {
+  //response.send('Hello World!');
+//});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+  //console.log("Listening on " + port);
+//});
